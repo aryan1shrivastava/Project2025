0bc2b47019f68fe7e55c75c0fe10bb319fb5ef0e
Aryan Shrivastava <aryanshrivastava@Aryans-MacBook-Air.local>
2025-10-11 19:08:24 +0530

    converted code to support tpescript

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import type { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/api/health", (req: Request, res: Response) => {
+  res.json({
+    status: "running",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`API listning on port ${port}`);
+});
